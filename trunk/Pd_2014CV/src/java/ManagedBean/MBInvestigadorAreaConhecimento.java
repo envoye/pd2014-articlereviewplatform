b80657fb0b2d5b5ef5afdebafe982452e018ba14
valterjd@gmail.com <valterjd@gmail.com@47fd9dda-7ce4-5fad-7ae6-3ffd0e858f83>
2014-07-24 21:02:22 +0000

    ConferenciaSubtema,conferenciaArtigo

---
+++ trunk/Pd_2014CV/src/java/ManagedBean/MBInvestigadorAreaConhecimento.java
@@ -0,0 +1,124 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ManagedBean;
+
+import HelpersHibernate.AllHellper;
+import HibernatePackage.Investigador;
+import HibernatePackage.Investigadorareaconhecimento;
+import HibernatePackage.Subtema;
+import java.util.List;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Valter
+ */
+@Named(value = "MBInvestigadorAreaConhecimento")
+@ViewScoped
+public class MBInvestigadorAreaConhecimento {
+    private Subtema subtema;
+    private Investigador investigador = new Investigador();
+    private Byte grauConfianca;
+    private Byte preferencia;
+    private List<Subtema> listaSubtemas;
+    private List<Investigador> listaInvestigadores;
+    @Inject
+    private LoginUtilizador loginUtilizador;
+     
+     
+    /**
+     * Creates a new instance of MBInvestigadorAreaConhecimento
+     */
+    public MBInvestigadorAreaConhecimento() {
+    }
+
+    public Subtema getSubtema() {
+        return subtema;
+    }
+
+    public void setSubtema(Subtema subtema) {
+        this.subtema = subtema;
+    }
+
+    public Investigador getInvestigador() {
+        return investigador;
+    }
+
+    public void setInvestigador(Investigador investigador) {
+        for(int i=0;i<this.getListaInvestigadores().size();i++){
+            if(this.listaInvestigadores.get(i).getId().equals(this.investigador.getId())){
+                this.investigador = this.listaInvestigadores.get(i);
+            }
+        }        
+    }
+
+    public Byte getGrauConfianca() {
+        return grauConfianca;
+    }
+
+    public void setGrauConfianca(Byte grauConfianca) {
+        this.grauConfianca = grauConfianca;
+    }
+
+    public Byte getPreferencia() {
+        return preferencia;
+    }
+
+    public void setPreferencia(Byte preferencia) {
+        this.preferencia = preferencia;
+    }
+
+    public List<Subtema> getListaSubtemas() {
+        if(this.listaSubtemas == null){
+            this.listaSubtemas = (List<Subtema>)AllHellper.getListQualquerCoisa(Subtema.class);
+        }                        
+        return listaSubtemas;
+    }
+
+    public void setListaSubtemas(List<Subtema> listaSubtemas) {
+        this.listaSubtemas = listaSubtemas;
+    }
+    
+    public List<Investigador> getListaInvestigadores() {
+        if(this.listaInvestigadores == null){
+            this.listaInvestigadores = (List<Investigador>)AllHellper.getListQualquerCoisa(Investigador.class);
+        }                
+        return listaInvestigadores;
+    }
+
+    public void setListaInvestigadores(List<Investigador> listaInvestigadores) {
+        this.listaInvestigadores = listaInvestigadores;
+    }
+
+    public LoginUtilizador getLoginUtilizador() {
+        return loginUtilizador;
+    }
+
+    public void setLoginUtilizador(LoginUtilizador loginUtilizador) {
+        this.loginUtilizador = loginUtilizador;
+    }
+    
+    public String introduzir() {
+        return "index";
+    }
+    
+    public String gravar() {
+        AllHellper.SaveQualquerCoisa(new Investigadorareaconhecimento(subtema, investigador, grauConfianca, preferencia));
+        return  "index";
+    }
+    
+    public String cancelar() {
+        return "index";
+    }
+
+    public String pesquisar() {
+        return "index";
+    }            
+    
+}
