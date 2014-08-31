3d0f1fe06a32b198c5a26d2ccdf43bd3af8d822d
carlos.fontes.tomas@gmail.com <carlos.fontes.tomas@gmail.com@47fd9dda-7ce4-5fad-7ae6-3ffd0e858f83>
2014-08-31 11:41:32 +0000

    chat,email

--- trunk/Pd_2014CV/src/java/HelpersHibernate/AllHellper.java
+++ trunk/Pd_2014CV/src/java/HelpersHibernate/AllHellper.java
@@ -5,6 +5,7 @@
  */
 package HelpersHibernate;
 
+import HibernatePackage.Investigador;
 import java.util.List;
 import org.hibernate.Query;
 import org.hibernate.Session;
@@ -38,46 +39,69 @@ public class AllHellper {
     public static List<? extends Object> getListQualquerCoisa(Class classObj) {
         Query q;
         try {
-        Session session = GeralHelper.GetSession(); //HibernateUtil.getSessionFactory().openSession();
-        org.hibernate.Transaction tx = session.beginTransaction();
-        q = session.createQuery("from " + classObj.getSimpleName());
-        }catch (Exception e) {
-           
+            Session session = GeralHelper.GetSession(); //HibernateUtil.getSessionFactory().openSession();
+            org.hibernate.Transaction tx = session.beginTransaction();
+            q = session.createQuery("from " + classObj.getSimpleName());
+        } catch (Exception e) {
+
             return null;
         }
         return q.list();
     }
 
     public static List<? extends Object> getListQualquerCoisaCondicao(Class classObj, String condicao) {
-         Query q;
+        Query q;
         try {
-        Session session = GeralHelper.GetSession(); //HibernateUtil.getSessionFactory().openSession();
-        org.hibernate.Transaction tx = session.beginTransaction();
-        q = session.createQuery("from " + classObj.getSimpleName() + condicao);
- }catch (Exception e) {
-           
+            Session session = GeralHelper.GetSession(); //HibernateUtil.getSessionFactory().openSession();
+            org.hibernate.Transaction tx = session.beginTransaction();
+            q = session.createQuery("from " + classObj.getSimpleName() + condicao);
+        } catch (Exception e) {
+
             return null;
         }
         return q.list();
     }
-public static List<? extends Object> getListArtigosInvestigador(String condicao) {
-         Query q;
+
+    public static List<? extends Object> getListArtigosInvestigador(String condicao) {
+        Query q;
         try {
-        Session session = GeralHelper.GetSession(); //HibernateUtil.getSessionFactory().openSession();
-        org.hibernate.Transaction tx = session.beginTransaction();
-        q = session.createQuery(condicao);
- }catch (Exception e) {
-           
+            Session session = GeralHelper.GetSession(); //HibernateUtil.getSessionFactory().openSession();
+            org.hibernate.Transaction tx = session.beginTransaction();
+            q = session.createQuery(condicao);
+        } catch (Exception e) {
+
             return null;
         }
         return q.list();
     }
+
     public static Object getQualquerCoisaCondicao(Class classObj, String condicao) {
         Session session = GeralHelper.GetSession(); //HibernateUtil.getSessionFactory().openSession();
         org.hibernate.Transaction tx = session.beginTransaction();
         Query q = session.createQuery("from " + classObj.getSimpleName() + condicao);
 
         return q.uniqueResult();
+    }
+
+    public static Boolean UpdateInvestigador(int idInvestigador) {
+        Session session = GeralHelper.GetSession();
+        org.hibernate.Transaction tx = null;
+        try {
+            tx = session.beginTransaction();
+            Investigador investigador
+                    = (Investigador) session.get(Investigador.class, idInvestigador);
+               investigador.setInstituicao(null);
+            session.update(investigador);
+
+            tx.commit();
+
+        } catch (Exception e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            return false;
+        }
+        return true;
     }
 
 }
