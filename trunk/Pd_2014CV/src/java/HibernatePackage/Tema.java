31541294bb928b7e7a2c551f4f7624c87c860c78
valterjd@gmail.com <valterjd@gmail.com@47fd9dda-7ce4-5fad-7ae6-3ffd0e858f83>
2014-09-02 15:48:45 +0000

    Pesquisas, Edição e Visualização para Temas

--- trunk/Pd_2014CV/src/java/HibernatePackage/Tema.java
+++ trunk/Pd_2014CV/src/java/HibernatePackage/Tema.java
@@ -19,18 +19,18 @@ public class Tema  implements java.io.Serializable {
 
     public Tema() {
     }
-
 	
     public Tema(String nomeTema) {
         this.nomeTema = nomeTema;
     }
+
     public Tema(String nomeTema, String descricao, Set<Conferenciaedicao> conferenciaedicaos, Set<Subtema> subtemas) {
        this.nomeTema = nomeTema;
        this.descricao = descricao;
        this.conferenciaedicaos = conferenciaedicaos;
        this.subtemas = subtemas;
     }
-   
+    
     public Integer getId() {
         return this.id;
     }
@@ -66,9 +66,6 @@ public class Tema  implements java.io.Serializable {
     public void setSubtemas(Set<Subtema> subtemas) {
         this.subtemas = subtemas;
     }
-
-
-
 
 }
 
