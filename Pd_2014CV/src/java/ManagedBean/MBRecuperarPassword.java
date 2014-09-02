a44f3cd10c3d40cb9e79c0e5a642c8de73a773b0
carlos.fontes.tomas <Carlos.fontes.tomas@gmail.com>
2014-09-02 14:20:22 +0000

    chat,email

--- Pd_2014CV/src/java/ManagedBean/MBRecuperarPassword.java
+++ Pd_2014CV/src/java/ManagedBean/MBRecuperarPassword.java
@@ -6,8 +6,14 @@
 
 package ManagedBean;
 
-import javax.inject.Named;
+import Email.SendEmail;
+import HelpersHibernate.AllHellper;
+import HibernatePackage.Investigador;
+import java.util.UUID;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
+import javax.inject.Named;
 
 /**
  *
@@ -18,7 +24,9 @@ import javax.faces.view.ViewScoped;
 public class MBRecuperarPassword {
 
    private String Email;
+   private Investigador investigador; 
 
+   
     public String getEmail() {
         return Email;
     }
@@ -26,5 +34,25 @@ public class MBRecuperarPassword {
     public void setEmail(String Email) {
         this.Email = Email;
     }
-    
+   public String recuperarPassword () {
+        
+        String condicao=(" as inv where inv.email='"+ Email+"'");
+
+        investigador=(Investigador)AllHellper.getQualquerCoisaCondicao(Investigador.class,condicao);
+
+        if (investigador!=null) {
+            UUID uuid = UUID.randomUUID();
+            String myRandom = uuid.toString().substring(0,10);
+            String message= "A sua nova passWord será:" + myRandom;
+            AllHellper.UpdatePassInvestigador(investigador.getId(), myRandom);
+            SendEmail.sendEmail(message,investigador.getEmail());
+            return "/model/investigador/LoginUtilizador.xhtml?faces-redirect=true";
+        }
+      
+        FacesMessage msg = new FacesMessage("O email não existe!", "ERROR MSG");
+        msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+
+        return "";
+    }  
 }
